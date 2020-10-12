b8b5e8bf3aefb3106ae39f0807deea64811bd40c
zhoujr01@mingyuanyun.com <764578566@163.com>
2020-10-12 11:27:46 +0800

    init

---
+++ ProcessManager/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ProcessManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+                Thread.Sleep(200);//让这个循环每次循环都等待0.2秒，这样达到实现效果：没0.2秒检查一次程序是否在运行
+
+                if (System.Diagnostics.Process.GetProcessesByName("ConsoleAppTest").ToList().Count > 0)
+                {
+                    Console.WriteLine("存在列表，不知运行状态");
+                }
+                else
+                {
+                    Console.WriteLine("不存在列表，该进程没有运行");
+                }
+            }
+        }
+    }
+}
